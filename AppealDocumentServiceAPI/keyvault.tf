d9b6c1d8cd68ee5be83f851afd8e1d12b72d111c
Ravi Kumar Addala <raviaddala9@gmail.com>
2021-11-03 15:52:34 +0000

    Create keyvault.tf

---
+++ AppealDocumentServiceAPI/keyvault.tf
@@ -0,0 +1,35 @@
+data "azurerm_client_config" "current" {}
+
+data "azurerm_key_vault" "key_vault" {
+  name                = var.key_vault_name
+  resource_group_name = "learning"
+}
+
+resource "azurerm_key_vault_access_policy" "myapp" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_app_service.app_service.identity[0].principal_id
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+
+}
+resource "azurerm_key_vault_access_policy" "myapp2" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_app_service.app_service2.identity[0].principal_id
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+
+}
