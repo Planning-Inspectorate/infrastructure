c25c4b6670f20095525e7e5bad2544487285c380
Ravi Kumar Addala <raviaddala9@gmail.com>
2021-11-03 15:53:05 +0000

    Create storage_accounts.tf

---
+++ AppealDocumentServiceAPI/storage_accounts.tf
@@ -0,0 +1,15 @@
+resource "azurerm_storage_account" "sa" {
+  name                     = "${var.storage_account_name}${random_string.rs.id}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = local.common_tags
+}
+
+resource "azurerm_storage_container" "sasc" {
+  name                  = var.stoage_account_container_name
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
