f9c789cee532da05493fa0a61699427e8ea8bb69
Ravi Kumar Addala <raviaddala9@gmail.com>
2021-11-16 10:15:39 +0000

    Update app_service_plan.tf

--- app_service_plan.tf
+++ app_service_plan.tf
@@ -3,14 +3,10 @@ resource "azurerm_app_service_plan" "asp" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind = "Linux"
-
+  reserved = true
   sku {
     tier = "Standard"
     size = "S1"
   }
-  properties{
-    reserved =true
-  }
-
   tags = local.common_tags
 }
