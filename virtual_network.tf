33bcaacc4992f5a02841089d1914697a597c8522
Ravi Kumar Addala <raviaddala9@gmail.com>
2021-11-17 16:18:23 +0000

    Update virtual_network.tf

--- virtual_network.tf
+++ virtual_network.tf
@@ -6,7 +6,7 @@ data "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnetname
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = "pins-uks-network-dev"
   virtual_network_name = data.azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.32.15.0/24"]
 
