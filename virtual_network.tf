439c28ffcbbed2211fcdedb15a109995a42e4ea2
Ravi Kumar Addala <raviaddala9@gmail.com>
2021-11-17 16:06:54 +0000

    Update virtual_network.tf

--- virtual_network.tf
+++ virtual_network.tf
@@ -4,11 +4,10 @@ data "azurerm_virtual_network" "vnet" {
   
 }
 
-
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnetname
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.32.15.0/24"]
 
   delegation {
