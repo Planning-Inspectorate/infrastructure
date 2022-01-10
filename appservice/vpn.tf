b85f650a3ed2f23fdb3c3366d4629ed42fcb6665
Rehan Anwar <rehananwar@Rehans-MacBook-Pro-2.local>
2022-01-10 15:30:32 +0000

    update connection name

--- appservice/vpn.tf
+++ appservice/vpn.tf
@@ -43,7 +43,7 @@ resource "azurerm_virtual_network_gateway" "vpn-gateway" {
 
 
 resource "azurerm_virtual_network_gateway_connection" "onpremise" {
-  name                = "onpremise"
+  name                = "uks-applications-nsip-vpn"
   location            = azurerm_resource_group.AppSrvRG.location
   resource_group_name = azurerm_resource_group.AppSrvRG.name
 
