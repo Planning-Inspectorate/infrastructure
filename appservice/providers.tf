af0f425601d2613dcc5fb0d346593b9b4f04de55
Rehan Anwar <rehananwar@Rehans-MacBook-Pro-2.local>
2022-01-13 18:30:06 +0000

    tfstate testing

--- appservice/providers.tf
+++ appservice/providers.tf
@@ -4,24 +4,16 @@ terraform {
     azurerm = {
       version = "~>2.75.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.1.0"
-    }
   }
 
   backend "azurerm" {
-    resource_group_name  = "pinsappealsterraforminfratest"
-    storage_account_name = "pinsappealsinfrastgtest"
-    container_name       = "pinsappealsinfracontainertest"
+    resource_group_name  = "pinsapplicationstfRG"
+    storage_account_name = "pinsapplicationstg"
+    container_name       = "pinsapplicationblob"
     key                  = "application.tfstate"
   }
 }
 provider "azurerm" {
   features {
   }
-}
-
-provider "random" {
-
 }
