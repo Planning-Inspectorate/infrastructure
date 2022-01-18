384649fef8520e07db3bc8ad1214b6807036df39
Ravi Kumar Addala <raviaddala9@gmail.com>
2022-01-18 21:50:43 +0000

    working on keyvault

--- lpa-questionnaire.tf
+++ lpa-questionnaire.tf
@@ -13,40 +13,40 @@ resource "azurerm_app_service" "app_service3" {
   }
 
   app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY"     = azurerm_application_insights.appinsights.instrumentation_key
-    "ALLOW_APPEAL_REPLY_CREATE"          = "true"
-    "APPEAL_REPLY_SERVICE_API_TIMEOUT"   = "10000"
-    "APPEAL_REPLY_SERVICE_API_URL"       = "https://pins-dev-appeal-reply-service-api-test7.azurewebsites.net"
-    "APPEALS_SERVICE_API_TIMEOUT"        = "10000"
-    "APPEALS_SERVICE_API_URL"            = "https://pins-dev-appeals-service-api-test7.azurewebsites.net"
-    "CLAM_AV_HOST"                       = "dev-clamav.azurewebsites.net"
-    "DOCKER_CUSTOM_IMAGE_NAME"           = "pinscommonukscontainers3887default.azurecr.io/lpa-questionnaire-web-app"
-    "DOCKER_REGISTRY_SERVER_PASSWORD"    = data.azurerm_key_vault_secret.docker-reg-server-pwd.value
-    "DOCKER_REGISTRY_SERVER_URL"         = data.azurerm_key_vault_secret.docker-reg-server-url.value
-    "DOCKER_REGISTRY_SERVER_USERNAME"    = data.azurerm_key_vault_secret.docker-reg-server-usr.value
-    "DOCUMENTS_SERVICE_API_TIMEOUT"      = "10000"
-    "DOCUMENTS_SERVICE_API_URL"          = "https://pins-dev-appeals-document-service-api-test7.azurewebsites.net"
-    "FILE_UPLOAD_DEBUG"                  = "false"
-    "FILE_UPLOAD_MAX_FILE_SIZE_BYTES"    = "15000000"
-    "FILE_UPLOAD_TMP_PATH"               = "/tmp"
-    "FILE_UPLOAD_USE_TEMP_FILES"         = "true"
-    "HOST_URL"                           = "https://pins-dev-lpa-questionnaire-test7.azurewebsites.net"
-    "JWT_SIGNING_KEY"                    = "QeThWmZq4t7w!z$C&F)J@NcRfUjXn2r5"
-    "LOGGER_LEVEL"                       = "debug"
+    "APPINSIGHTS_INSTRUMENTATIONKEY"           = azurerm_application_insights.appinsights.instrumentation_key
+    "ALLOW_APPEAL_REPLY_CREATE"                = "true"
+    "APPEAL_REPLY_SERVICE_API_TIMEOUT"         = "10000"
+    "APPEAL_REPLY_SERVICE_API_URL"             = "https://pins-dev-appeal-reply-service-api-test7.azurewebsites.net"
+    "APPEALS_SERVICE_API_TIMEOUT"              = "10000"
+    "APPEALS_SERVICE_API_URL"                  = "https://pins-dev-appeals-service-api-test7.azurewebsites.net"
+    "CLAM_AV_HOST"                             = "dev-clamav.azurewebsites.net"
+    "DOCKER_CUSTOM_IMAGE_NAME"                 = "pinscommonukscontainers3887default.azurecr.io/lpa-questionnaire-web-app"
+    "DOCKER_REGISTRY_SERVER_PASSWORD"          = data.azurerm_key_vault_secret.docker-reg-server-pwd.value
+    "DOCKER_REGISTRY_SERVER_URL"               = data.azurerm_key_vault_secret.docker-reg-server-url.value
+    "DOCKER_REGISTRY_SERVER_USERNAME"          = data.azurerm_key_vault_secret.docker-reg-server-usr.value
+    "DOCUMENTS_SERVICE_API_TIMEOUT"            = "10000"
+    "DOCUMENTS_SERVICE_API_URL"                = "https://pins-dev-appeals-document-service-api-test7.azurewebsites.net"
+    "FILE_UPLOAD_DEBUG"                        = "false"
+    "FILE_UPLOAD_MAX_FILE_SIZE_BYTES"          = "15000000"
+    "FILE_UPLOAD_TMP_PATH"                     = "/tmp"
+    "FILE_UPLOAD_USE_TEMP_FILES"               = "true"
+    "HOST_URL"                                 = "https://pins-dev-lpa-questionnaire-test7.azurewebsites.net"
+    "JWT_SIGNING_KEY"                          = "QeThWmZq4t7w!z$C&F)J@NcRfUjXn2r5"
+    "LOGGER_LEVEL"                             = "debug"
     "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET" = data.azurerm_key_vault.secret.microsoft-pro-auth-lpa-secret.value
-    "NODE_ENV"                           = "production"
-    "PDF_SERVICE_API_URL"                = "https://pins-dev-pdf-service-api-test7.azurewebsites.net"
-    "PORT"                               = "3000"
-    "SESSION_KEY"                        = "some_secure_key_goes_here"
-    "SESSION_MONGODB_COLLECTION"         = "sessions"
-    "SESSION_MONGODB_DB_NAME"            = "lpa-questionnaire-web-app"
-    "SESSION_MONGODB_URL"                = "mongodb://pins-uks-mongodb-9475-dev:MjTJyWvTBUhVjSztbpx3eySEB7VCGekVPAzdqdWaAuxqwQApkRi1WYldeMFaDTYaQlYuauB31pwrWVBd5d1YDA==@pins-uks-mongodb-9475-dev.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@pins-uks-mongodb-9475-dev@"
-    "USE_SECURE_SESSION_COOKIES"         = "false"
-    "TOKEN_COOKIE_NAME"                  = "authCookie"
-    "TOKEN_VALIDITY_MILLIS"              = "144400000"
-    "USE_SECURE_SESSION_COOKIES"          = "false"
-    "WEBSITE_HTTPLOGGING_RETENTION_DAYS" = "28"
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE  = "false"
+    "NODE_ENV"                                 = "production"
+    "PDF_SERVICE_API_URL"                      = "https://pins-dev-pdf-service-api-test7.azurewebsites.net"
+    "PORT"                                     = "3000"
+    "SESSION_KEY"                              = "some_secure_key_goes_here"
+    "SESSION_MONGODB_COLLECTION"               = "sessions"
+    "SESSION_MONGODB_DB_NAME"                  = "lpa-questionnaire-web-app"
+    "SESSION_MONGODB_URL"                      = "mongodb://pins-uks-mongodb-9475-dev:MjTJyWvTBUhVjSztbpx3eySEB7VCGekVPAzdqdWaAuxqwQApkRi1WYldeMFaDTYaQlYuauB31pwrWVBd5d1YDA==@pins-uks-mongodb-9475-dev.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@pins-uks-mongodb-9475-dev@"
+    "USE_SECURE_SESSION_COOKIES"               = "false"
+    "TOKEN_COOKIE_NAME"                        = "authCookie"
+    "TOKEN_VALIDITY_MILLIS"                    = "144400000"
+    "USE_SECURE_SESSION_COOKIES"               = "false"
+    "WEBSITE_HTTPLOGGING_RETENTION_DAYS"       = "28"
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE        = "false"
   }
 }
 
