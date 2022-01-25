700d8a4401d8c3a13005500a5fa7af7f2f41c353
Rehan Anwar <rehananwar@Rehans-MacBook-Pro-2.local>
2022-01-25 20:33:00 +0000

    vpn connection testing

--- appservice/vpn.tf
+++ appservice/vpn.tf
@@ -20,7 +20,7 @@ resource "azurerm_virtual_network_gateway" "vpn-gateway" {
   resource_group_name = azurerm_resource_group.AppSrvRG.name
   type                = "Vpn"
   vpn_type            = "RouteBased"
-  sku                 = "VpnGw1"
+  sku                 = "VpnGw2"
   # private_ip_address_enabled = true
 
   ip_configuration {
